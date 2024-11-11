b7fb97850691d954164a8519afec5cefa8dd07a3
John <johnlin8968@gmail.com>
2024-11-11 17:10:36 +1100

    Update Hero.jsx

---
+++ components/Hero.jsx
@@ -0,0 +1,31 @@
+/**
+ * @copyright 2024 John Lin
+ * @license Apache-2.0
+ */
+
+const Hero = () => {
+  return (
+
+<section id="home" className="">
+
+    <div className="">
+        <div>
+            <div className="">
+                <figure className="">
+                    <img src="/images/avatar-1.jpg" width={40} height={40} alt="John Lin Portrait" 
+                    className="img-cover" />
+                </figure>
+
+                <div className="">
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+  )
+}
+
+export default Hero
