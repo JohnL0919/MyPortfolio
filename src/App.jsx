d0a921143cb56722401bfdc1aea1403ab7fcf721
John <johnlin8968@gmail.com>
2024-11-13 14:40:46 +1100

    Update Skill & SkillCard

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,20 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Skill from "../components/Skill";
 
 const App = () => {
-    return ( 
+  return (
     <>
-        <Header/>
-        
-        <main>
-            <Hero/>
-            <About/>
-        </main>
-    </>
-    )
+      <Header />
 
-}
+      <main>
+        <Hero />
+        <About />
+        <Skill />
+      </main>
+    </>
+  );
+};
 
 export default App;
