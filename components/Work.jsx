ea8c602a2ebeeb6261f0b6f570eede37f282f903
John <johnlin8968@gmail.com>
2024-11-15 00:40:20 +1100

    Update work.jsx and projectcard.jsx
    Some minor styling issues that requires fixing (inc. hover effects...)

--- components/Work.jsx
+++ components/Work.jsx
@@ -3,6 +3,11 @@
  * @license Apache-2.0
  */
 
+/**
+ * Components
+ */
+import ProjectCard from "./ProjectCard";
+
 const works = [
   {
     imgSrc: "/images/project-1.jpg",
@@ -43,17 +48,25 @@ const works = [
 ];
 
 const Work = () => {
-  <section id="work" className="section">
-    <div className="container">
-      <h2 className="headline-2 mb-8">My portfolio highlights</h2>
+  return (
+    <section id="work" className="section">
+      <div className="container">
+        <h2 className="headline-2 mb-8">My portfolio highlights</h2>
 
-      <div className="">
-        {works.map(
-          ({ imgSrc, title, tags, projectLink }, key) => "ProjectCard"
-        )}
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
+          {works.map(({ imgSrc, title, tags, projectLink }, key) => (
+            <ProjectCard
+              key={key}
+              imgSrc={imgSrc}
+              title={title}
+              tags={tags}
+              projectLink={projectLink}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  </section>;
+    </section>
+  );
 };
 
 export default Work;
