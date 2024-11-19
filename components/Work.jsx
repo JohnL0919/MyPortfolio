880fea4bd650048f61f9cfbb7e6f4baf1eef50fa
John <johnlin8968@gmail.com>
2024-11-20 10:52:08 +1100

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -11,39 +11,39 @@ import ProjectCard from "./ProjectCard";
 const works = [
   {
     imgSrc: "/images/project-1.jpg",
-    title: "Full stack music app",
+    title: "PlaceHolder",
     tags: ["API", "MVC", "Development"],
-    projectLink: "https://musify-5al0.onrender.com/",
+    projectLink: "/",
   },
   {
     imgSrc: "/images/project-2.jpg",
-    title: "Free stock photo app",
+    title: "PlaceHolder",
     tags: ["API", "SPA"],
-    projectLink: "https://pixstock-official.vercel.app/",
+    projectLink: "/",
   },
   {
     imgSrc: "/images/project-3.jpg",
-    title: "Recipe app",
+    title: "PlaceHolder",
     tags: ["Development", "API"],
     projectLink: "",
   },
   {
     imgSrc: "/images/project-4.jpg",
-    title: "Real state website",
+    title: "PlaceHolder",
     tags: ["Web-design", "Development"],
-    projectLink: "https://github.com/codewithsadee-org/wealthome",
+    projectLink: "/",
   },
   {
     imgSrc: "/images/project-5.jpg",
-    title: "eCommerce website",
+    title: "PlaceHolder",
     tags: ["eCommerce", "Development"],
-    projectLink: "https://github.com/codewithsadee/anon-ecommerce-website",
+    projectLink: "/",
   },
   {
     imgSrc: "/images/project-6.jpg",
-    title: "vCard Personal portfolio",
+    title: "PlaceHolder",
     tags: ["Web-design", "Development"],
-    projectLink: "https://github.com/codewithsadee/vcard-personal-portfolio",
+    projectLink: "/",
   },
 ];
 
